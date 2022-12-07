54f376de7cdda3de029f71359ffdcbfa24e75599
Blake Dewey <blake.dewey@jhu.edu>
2022-12-07 08:34:59 -0500

    move logic, just a bit faster

--- 2022/day6.py
+++ 2022/day6.py
@@ -3,16 +3,18 @@ def main(input_filename):
     marker = []
     marker_len = 4
     positions = []
-    for i,char in enumerate(datastream):
-        if char in marker:
-            marker = []
-        marker.append(char)
-        if len(marker) == marker_len:
-            positions.append(i+1)
-            if marker_len == 4:
-                marker_len = 14
-            else:
-                return positions
+    for i, char in enumerate(datastream):
+        if char not in marker:
+            marker.append(char)
+            if len(marker) == marker_len:
+                positions.append(i+1)
+                if marker_len == 4:
+                    marker_len = 14
+                else:
+                   return positions
+        else:
+            marker = [char]
+        
 
 if __name__ == '__main__':
-    main('data/day6.txt')
+    print(main('data/day6.txt'))
